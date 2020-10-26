1a39a0b85db741aa085c6d0d5217bac12e7bf93b
Eric C <39015447+ricetech@users.noreply.github.com>
2020-10-26 19:00:48 -0400

    Initial Commit

---
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.gng2101groupb32.pathfindr.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
