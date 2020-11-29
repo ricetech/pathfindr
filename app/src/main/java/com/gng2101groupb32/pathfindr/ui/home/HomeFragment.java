7909b1b7b649e7f3b2c819dd74aa9af20ea5af98
Eric C <39015447+ricetech@users.noreply.github.com>
2020-11-29 17:04:34 -0500

    Add buttons to home screen

--- app/src/main/java/com/gng2101groupb32/pathfindr/ui/home/HomeFragment.java
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/home/HomeFragment.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import com.gng2101groupb32.pathfindr.R;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button btnAnnouncements;
+    private Button btnNavigate;
+    private Button btnLocInfo;
+    private Button btnHelp;
+    private Button btnSettings;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +33,41 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+        btnAnnouncements = root.findViewById(R.id.home_button_announcements);
+        btnNavigate = root.findViewById(R.id.home_button_navigate);
+        btnLocInfo = root.findViewById(R.id.home_button_loc_info);
+        btnHelp = root.findViewById(R.id.home_button_help);
+        btnSettings = root.findViewById(R.id.home_button_settings);
+
+        btnAnnouncements.setOnClickListener(view -> {
+            NavDirections directions = HomeFragmentDirections
+                    .actionNavigationHomeToNavigationAnnouncements();
+            Navigation.findNavController(root).navigate(directions);
+        });
+
+        btnNavigate.setOnClickListener(view -> {
+            NavDirections directions = HomeFragmentDirections
+                    .actionNavigationHomeToNavigationNavList();
+            Navigation.findNavController(root).navigate(directions);
+        });
+
+        btnLocInfo.setOnClickListener(view -> {
+            NavDirections directions = HomeFragmentDirections
+                    .actionNavigationHomeToNavigationLocList();
+            Navigation.findNavController(root).navigate(directions);
+        });
+
+        btnHelp.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "Error: This feature is not implemented yet!",
+                           Toast.LENGTH_LONG).show();
+        });
+
+        btnSettings.setOnClickListener(view -> {
+            NavDirections directions = HomeFragmentDirections
+                    .actionNavigationHomeToNavigationSettings();
+            Navigation.findNavController(root).navigate(directions);
+        });
+
         return root;
     }
 }
