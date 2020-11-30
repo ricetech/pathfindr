30c381941cfc8db17cdd8535e260f06e09fc2fb3
Eric C <39015447+ricetech@users.noreply.github.com>
2020-11-29 19:07:06 -0500

    Fix toolbar back button not working

--- app/src/main/java/com/gng2101groupb32/pathfindr/MainActivity.java
+++ app/src/main/java/com/gng2101groupb32/pathfindr/MainActivity.java
@@ -11,6 +11,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_settings, R.id.navigation_announcements,
+            R.id.navigation_nav_list, R.id.navigation_loc_list)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,16 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_settings, R.id.navigation_announcements,
-                R.id.navigation_nav_list, R.id.navigation_loc_list)
-                .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, this.appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
