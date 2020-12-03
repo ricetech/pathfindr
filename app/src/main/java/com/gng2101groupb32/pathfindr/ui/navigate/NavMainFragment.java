c9fec95f9c3d7da24362b6b8f3727439832fe239
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-03 00:04:23 -0500

    Add Beacon boilerplate code to NavMainFragment

--- app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavMainFragment.java
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavMainFragment.java
@@ -1,6 +1,11 @@
 package com.gng2101groupb32.pathfindr.ui.navigate;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +14,23 @@ import androidx.fragment.app.Fragment;
 
 import com.gng2101groupb32.pathfindr.R;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
 /**
  * A simple {@link Fragment} subclass. Use the {@link NavMainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NavMainFragment extends Fragment {
+public class NavMainFragment extends Fragment implements BeaconConsumer {
+    public static final String TAG = "NavMainFragment";
+    private BeaconManager beaconManager;
+
     public NavMainFragment() {
         // Required empty public constructor
     }
@@ -34,6 +51,11 @@ public class NavMainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        beaconManager = BeaconManager.getInstanceForApplication(requireContext());
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(
+                "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+        ));
+        beaconManager.bind(this);
     }
 
     @Override
@@ -41,5 +63,44 @@ public class NavMainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nav_main, container, false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.removeAllRangeNotifiers();
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about " + beacons.iterator().next().getDistance() + " meters away.");
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+        }
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public boolean bindService(Intent intent, ServiceConnection serviceConnection, int i) {
+        return false;
     }
 }
