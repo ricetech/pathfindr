3aded242085958d785e2564feda08fb6bcfc9384
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-02 21:15:21 -0500

    Create NavigateListener

---
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/location_info/NavigateListener.java
@@ -0,0 +1,7 @@
+package com.gng2101groupb32.pathfindr.ui.location_info;
+
+import com.gng2101groupb32.pathfindr.db.Location;
+
+public interface NavigateListener {
+    void onLocationNavigateSelect(Location location);
+}
