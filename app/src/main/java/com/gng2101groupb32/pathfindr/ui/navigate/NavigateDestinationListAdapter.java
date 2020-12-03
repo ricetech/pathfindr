8c83859b253b48648c1663fbe17d31b66a3f4aff
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-02 19:06:07 -0500

    Connect NavigateDestinationListFragment to LocationViewFragment

--- app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavigateDestinationListAdapter.java
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavigateDestinationListAdapter.java
@@ -1,68 +1,85 @@
 package com.gng2101groupb32.pathfindr.ui.navigate;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.gng2101groupb32.pathfindr.R;
-import com.gng2101groupb32.pathfindr.dummy.DummyContent.DummyItem;
+import com.gng2101groupb32.pathfindr.db.Location;
+import com.gng2101groupb32.pathfindr.ui.location_info.LocationsListener;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.ref.WeakReference;
+import java.util.Collections;
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem}.
- * TODO: Replace the implementation with code for your data type.
- */
 public class NavigateDestinationListAdapter extends RecyclerView.Adapter<NavigateDestinationListAdapter.ViewHolder> {
+    private final List<Location> locations;
+    private final LocationsListener listener;
 
-    private final List<DummyItem> mValues;
-
-    public NavigateDestinationListAdapter(List<DummyItem> items) {
-        mValues = items;
+    public NavigateDestinationListAdapter(List<Location> locations, LocationsListener listener) {
+        this.locations = locations;
+        this.listener = listener;
     }
 
     @NotNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_navigate_destination_list_item, parent, false);
+                                  .inflate(R.layout.fragment_navigate_destination_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        Collections.sort(locations, (loc1, loc2) -> loc1.getName().compareTo(loc2.getName()));
+        holder.location = locations.get(position);
+        holder.nameTV.setText(locations.get(position).getName());
+        holder.descriptionTV.setText(locations.get(position).getDescription());
     }
 
     @Override
     public int getItemCount() {
-        return mValues.size();
+        return locations.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    @SuppressWarnings("FieldCanBeLocal")
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public DummyItem mItem;
+        public final TextView nameTV;
+        public final TextView descriptionTV;
+
+        private final Button btnInfo;
+        private final WeakReference<LocationsListener> listenerRef;
+
+        public Location location;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
-            mContentView = (TextView) view.findViewById(R.id.content);
+            nameTV = (TextView) view.findViewById(R.id.nav_list_name);
+            descriptionTV = (TextView) view.findViewById(R.id.nav_list_description);
+
+            btnInfo = this.mView.findViewById(R.id.nav_list_info);
+            btnInfo.setOnClickListener(this);
+
+            listenerRef = new WeakReference<>(listener);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listenerRef.get().onLocationClick(location);
         }
 
         @NotNull
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + descriptionTV.getText() + "'";
         }
     }
 }
--- app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavigateDestinationListFragment.java
+++ app/src/main/java/com/gng2101groupb32/pathfindr/ui/navigate/NavigateDestinationListFragment.java
@@ -2,37 +2,42 @@ package com.gng2101groupb32.pathfindr.ui.navigate;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.gng2101groupb32.pathfindr.R;
-import com.gng2101groupb32.pathfindr.dummy.DummyContent;
+import com.gng2101groupb32.pathfindr.db.Location;
+import com.gng2101groupb32.pathfindr.ui.location_info.LocationViewModel;
+import com.gng2101groupb32.pathfindr.ui.location_info.LocationsListener;
 
 /**
  * A fragment representing a list of Items.
  */
-public class NavigateDestinationListFragment extends Fragment {
+public class NavigateDestinationListFragment extends Fragment implements LocationsListener {
 
-    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 1;
 
+    private LocationViewModel viewModel;
+
     /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment (e.g. upon
+     * screen orientation changes).
      */
     public NavigateDestinationListFragment() {
     }
 
-    // TODO: Customize parameter initialization
     public static NavigateDestinationListFragment newInstance(int columnCount) {
         NavigateDestinationListFragment fragment = new NavigateDestinationListFragment();
         Bundle args = new Bundle();
@@ -64,8 +69,31 @@ public class NavigateDestinationListFragment extends Fragment {
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new NavigateDestinationListAdapter(DummyContent.ITEMS));
+            Location.getLiveLocations((locations, error) -> recyclerView.setAdapter(
+                    new NavigateDestinationListAdapter(locations, this)
+            ));
+            // Add Dividers
+            recyclerView.addItemDecoration(new DividerItemDecoration(requireContext(),
+                                                                     DividerItemDecoration.VERTICAL));
         }
+        // Init ViewModel
+        viewModel = new ViewModelProvider((ViewModelStoreOwner) requireActivity())
+                .get(LocationViewModel.class);
         return view;
+    }
+
+    @Override
+    public void onLocationClick(Location location) {
+        // Set selected Location
+        viewModel.select(location);
+
+        // Navigate to fragment
+        NavDirections directions = NavigateDestinationListFragmentDirections.actionNavigationNavListToLocationViewFragment();
+        Navigation.findNavController(requireView()).navigate(directions);
+    }
+
+    @Override
+    public void onLocationDelete(Location location) {
+
     }
 }
